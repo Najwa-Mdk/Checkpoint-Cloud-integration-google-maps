435fa96907836e0f4e87cf83e4b1282924803dc0
Najwa-Mdk <116720508+Najwa-Mdk@users.noreply.github.com>
2023-01-02 16:54:35 +0100

    first commit

---
+++ src/Components/MovieCard/MovieCard.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import {Card, Button} from 'react-bootstrap';
+import './MovieCard.css';
+import MovieRating from '../Rating/Rating' ;
+
+const MovieCard = ({movie}) => {
+  return (
+    <div className='card-movie'>
+         <Card style={{ width: '18rem' }}>
+      <Card.Img variant="top" src={movie.posterUrl} />
+      <Card.Body>
+        <Card.Title>{movie.title}</Card.Title>
+        <Card.Text>{movie.rate}</Card.Text>
+        <MovieRating isMovieRating = {true} movieRating = {movie.rate}/>
+        <Button className = 'btn'variant="primary">Go somewhere</Button>
+      </Card.Body>
+    </Card>
+    </div>
+  );
+};
+
+export default MovieCard
